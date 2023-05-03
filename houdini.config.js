b1138f733f476d3048ba3610e5300b0e4915a6e2
Nicholas Gates <nickcgates@outlook.com>
2023-05-02 20:11:49 -0400

    init commit

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://rickandmortyapi.com/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+    /* @type { import('./$houdini').PageData } */
+    export let data
+
+    console.log(`data`, data)
+
+    $: ({ AllCharacters } = data)
+
+    // console.log(data.AllCharacters)
+
+</script>
+
+
+{#if $AllCharacters.fetching}
+    <div>Loading...</div>
+{:else}
+    <div>
+    <!-- { JSON.stringify($AllCharacters.data)} -->
+    {#each $AllCharacters.data.characters.results as character}
+      <section>
+        <a data-sveltekit-prefetch href={`/character/${character?.id}`}>
+            <img src={character?.image} alt={character?.name} />
+            <h2>{character?.name}</h2>
+        </a>
+      </section>
+    {/each}
+    </div>
+{/if}
+
