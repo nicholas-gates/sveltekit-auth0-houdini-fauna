5611f52841aa57dceba5d5b6994975d4c6b7c995
Nicholas Gates <nickcgates@outlook.com>
2023-05-03 11:45:12 -0400

    added faunadb graphql page

--- houdini.config.js
+++ houdini.config.js
@@ -2,9 +2,13 @@
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
-    "watchSchema": {
-        "url": "https://rickandmortyapi.com/graphql"
-    },
+    // "watchSchema": {
+        // "url": "https://rickandmortyapi.com/graphql"
+        // "url": "https://graphql.us.fauna.com/graphql",
+        // "headers": {
+        //     Authorization: `Bearer ${FAUNADB_KEY}`
+        // }
+    // },
     "plugins": {
         "houdini-svelte": {}
     }
